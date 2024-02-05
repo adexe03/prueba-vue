466f2a260cf0a92ae5279a5ccb932d63cb1984e3
Adexe Reyes <adexe.reyes.cb@gmail.com>
2024-02-05 18:36:14 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template lang="">
+  <h1>Lista de tareas</h1>
+  <p>Se han encontrado {{tasks.length}} tarea/s</p>
+  <p>{{ textoNumeroTareas }}</p>
+  <ul>
+    <li v-for='task of tasks' :key='task.id'>{{task.id}} - {{task.name}}</li>
+  </ul>
+</template>
+<script setup>
+import { computed, ref } from 'vue';
+
+const tasks = ref([
+  { id: 1, name: 'Corregir exámenes del lado del servidor' },
+  { id: 2, name: 'Corregir exámenes del lado del cliente' },
+  { id: 3, name: 'Corregir exámenes de recuperación' },
+  { id: 4, name: 'Preparar examen chungo de Vue' },
+]);
+
+const textoNumeroTareas = computed(() => {
+  if (tasks.value.length == 0) return "No hay tareas";
+  else if (tasks.value.length == 1) return "Hay una tarea";
+  return "Hay " + tasks.value.length + " tareas";
+});
+</script>
+<style lang="">
+  
+</style>
